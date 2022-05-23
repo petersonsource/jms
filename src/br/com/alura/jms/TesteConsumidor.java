27d6b33cf352c3bc5967410b9d1d98943bbd3236
unknown <peterson.opensource@hotmail.com>
2022-05-22 23:38:41 -0300

    adding message listener

--- src/br/com/alura/jms/TesteConsumidor.java
+++ src/br/com/alura/jms/TesteConsumidor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -24,11 +27,24 @@ public class TesteConsumidor {
 		Destination fila = (Destination) context.lookup("financeiro");	
 		MessageConsumer consumer = session.createConsumer(fila);
 		
-		Message mensagem = consumer.receive();
-		
-		System.out.println("recebendo mensaem " + mensagem);
-		
-		//new Scanner(System.in).nextLine();
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				
+				TextMessage textMessage = (TextMessage) message;
+				
+				
+				try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+			
+		new Scanner(System.in).nextLine();
 
 		connection.close();
 		context.close();
